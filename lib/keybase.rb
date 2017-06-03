ab4015e18f925fa541b5cb06e7a3d191ea0dacf7
William Woodruff <william@tuffbizz.com>
2017-06-02 23:10:59 -0400

    keybase: Bump to 0.0.7.

--- lib/keybase.rb
+++ lib/keybase.rb
@@ -11,7 +11,7 @@ require_relative "keybase/chat"
 # The primary namespace for keybase-unofficial.
 module Keybase
   # keybase-unofficial's current version
-  VERSION = "0.0.6"
+  VERSION = "0.0.7"
 
   extend Configuration
 end
